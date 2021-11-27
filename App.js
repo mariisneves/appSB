bef8926d1dbb5b97a2f50ec96e02ed52ac69a835
mariisneves <mariana.aneves@outlook.com>
2021-11-27 00:59:46 -0300

    Tela cadastro v2

--- App.js
+++ App.js
@@ -1,81 +1,84 @@
+import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Switch, Chechbox, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Switch, Chechbox, TextInput, TouchableOpacity, Image } from 'react-native';
 import styles from './assets/estilo';
+import btnvoltar from './assets/btn-volta.png';
 import firebase from './assets/firebaseConnect';
-import AppLoading from 'expo-app-loading';
+import AppLoading from 'expo';
 import {
   useFonts, SourceSansPro_300Light, SourceSansPro_300Light_Italic, SourceSansPro_400Regular,
   SourceSansPro_400Regular_Italic, SourceSansPro_600SemiBold, SourceSansPro_600SemiBold_Italic,
   SourceSansPro_700Bold,
 } from '@expo-google-fonts/source-sans-pro';
 
-// export default () => {
-//   let [fontsLoaded] = useFonts({
-//     SourceSansPro_300Light,
-//     SourceSansPro_300Light_Italic,
-//     SourceSansPro_400Regular,
-//     SourceSansPro_400Regular_Italic,
-//     SourceSansPro_600SemiBold,
-//     SourceSansPro_600SemiBold_Italic,
-//     SourceSansPro_700Bold,
-//   });
+// export default function App() {
+// let [fontsLoaded] = useFonts({
+//   SourceSansPro_300Light,
+//   SourceSansPro_300Light_Italic,
+//   SourceSansPro_400Regular,
+//   SourceSansPro_400Regular_Italic,
+//   SourceSansPro_600SemiBold,
+//   SourceSansPro_600SemiBold_Italic,
+//   SourceSansPro_700Bold,
+// });
 
-  export default class App extends Component {
+export default class App extends Component {
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        nome: '',
-        email: '',
-        telefone: '',
-        endereco: '',
-        numero: '',
-        complemento: '',
-        cidade: '',
-        estado: '',
-        cep: '',
-        senha: '',
-        senha_rep: '',
-        termos: false,
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: '',
+      email: '',
+      telefone: '',
+      endereco: '',
+      numero: '',
+      complemento: '',
+      cidade: '',
+      estado: '',
+      cep: '',
+      senha: '',
+      senha_rep: '',
+      termos: false,
+    };
 
-      this.enviarDados = this.enviarDados.bind(this);
+    this.enviarDados = this.enviarDados.bind(this);
 
+  }
+
+  //Metodo que é chamado quando você clica no botao Abrir Conta
+  enviarDados() {
+
+    if (this.state.nome === '' || this.state.email === '') {
+      alert('Preencha todos dados corretamente!')
+      return;
     }
 
-    //Metodo que é chamado quando você clica no botao Abrir Conta
-    enviarDados() {
+    alert(
+      'Cadastro feito com sucesso! \n\n' +
+      'Nome: ' + this.state.nome + '\n' +
+      'E-mail: ' + this.state.email + '\n' +
+      'Telefone: ' + this.state.telefone + ' \n' +
+      'Endereço: ' + this.state.endereco + ' \n' +
+      'Nº: ' + this.state.numero + ' \n' +
+      'Complemento: ' + this.state.complemento + ' \n' +
+      'Cidade: ' + this.state.cidade + ' \n' +
+      'Estado: ' + this.state.estado + ' \n' +
+      'CEP: ' + this.state.cep + ' \n' +
+      'Senha: ' + this.state.senha + ' \n' +
+      'Repetir a senha: ' + this.state.senha_rep + ' \n' +
+      'Termos: ' + ((this.state.termos) ? 'Aceito' : 'Não aceito')
+    );
 
-      if (this.state.nome === '' || this.state.email === '') {
-        alert('Preencha todos dados corretamente!')
-        return;
-      }
-
-      alert(
-        'Cadastro feito com sucesso! \n\n' +
-        'Nome: ' + this.state.nome + '\n' +
-        'E-mail: ' + this.state.email + '\n' +
-        'Telefone: ' + this.state.telefone + ' \n' +
-        'Endereço: ' + this.state.endereco + ' \n' +
-        'Nº: ' + this.state.numero + ' \n' +
-        'Complemento: ' + this.state.complemento + ' \n' +
-        'Cidade: ' + this.state.cidade + ' \n' +
-        'Estado: ' + this.state.estado + ' \n' +
-        'CEP: ' + this.state.cep + ' \n' +
-        'Senha: ' + this.state.senha + ' \n' +
-        'Repetir a senha: ' + this.state.senha_rep + ' \n' +
-        'Termos: ' + ((this.state.termos) ? 'Aceito' : 'Não aceito')
-      );
-
-    }
+  }
 
 
-    render() {
+  render() {
 
-      return (
-        <View style={styles.container}>
+    return (
+      <View style={styles.container}>
+        <Image source={btnvoltar} style={styles.btnvoltar} />
+        <View style={styles.containerFormulario}>
           <Text style={styles.title}>Preencha os campos abaixo para completar o seu cadastro</Text>
-
           <View style={styles.areaFormulario}>
 
             <View>
@@ -103,29 +106,33 @@ import {
                 onChangeText={(texto) => this.setState({ endereco: texto })}
               />
 
-              <TextInput style={styles.input}
-                placeholder="Nº"
-                underlineColorAndroid="transparent"
-                onChangeText={(texto) => this.setState({ numero: texto })}
-              />
+              <View style={styles.doisForms}>
+                <TextInput style={styles.inputDois}
+                  placeholder="Nº"
+                  underlineColorAndroid="transparent"
+                  onChangeText={(texto) => this.setState({ numero: texto })}
+                />
 
-              <TextInput style={styles.input}
-                placeholder="Complemento"
-                underlineColorAndroid="transparent"
-                onChangeText={(texto) => this.setState({ complemento: texto })}
-              />
+                <TextInput style={styles.inputDois}
+                  placeholder="Complemento"
+                  underlineColorAndroid="transparent"
+                  onChangeText={(texto) => this.setState({ complemento: texto })}
+                />
+              </View>
 
-              <TextInput style={styles.input}
-                placeholder="Cidade"
-                underlineColorAndroid="transparent"
-                onChangeText={(texto) => this.setState({ cidade: texto })}
-              />
+              <View style={styles.doisForms}>
+                <TextInput style={styles.inputDois}
+                  placeholder="Cidade"
+                  underlineColorAndroid="transparent"
+                  onChangeText={(texto) => this.setState({ cidade: texto })}
+                />
 
-              <TextInput style={styles.input}
-                placeholder="Estado"
-                underlineColorAndroid="transparent"
-                onChangeText={(texto) => this.setState({ estado: texto })}
-              />
+                <TextInput style={styles.inputDois}
+                  placeholder="Estado"
+                  underlineColorAndroid="transparent"
+                  onChangeText={(texto) => this.setState({ estado: texto })}
+                />
+              </View>
 
               <TextInput style={styles.input}
                 placeholder="CEP"
@@ -147,28 +154,26 @@ import {
 
             </View>
 
-            <View style={styles.checkboxTermos}>
+            <View style={styles.switchTermos}>
               <Switch
                 style={{ paddingTop: 15 }}
-                trackColor="#00c300"
-                value={this.state.estudante}
-                onValueChange={(valorEstudante) => this.setState({ estudante: valorEstudante })}
+                trackColor={{ false: "#767577", true: "#5B352C" }}
+                value={this.state.termos}
+                onValueChange={(valorTermos) => this.setState({ termos: valorTermos })}
               />
-              <Text style={styles.textoNome}>Termos</Text>
+              <Text style={styles.textoNome}>Li e aceito os termos e condições.</Text>
             </View>
-
-
 
             <TouchableOpacity style={styles.botao} onPress={this.enviarDados} underlayColor="#D8A35D">
               <Text style={styles.botaoTexto}>Finalizar</Text>
             </TouchableOpacity>
 
-
           </View>
-
         </View>
-      );
 
-    }
+      </View >
+    );
 
   }
+
+}
--- assets/estilo.js
+++ assets/estilo.js
@@ -1,59 +1,92 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 
-export  default StyleSheet.create({
+export default StyleSheet.create({
 
-    container: {
-        paddingTop: 20,
-        marginHorizontal: 35,
-        flex: 1,
-      },
-  
-      areaFormulario: {
-        flexDirection: 'column',
-      },
-  
-      title: {
-        textAlign: 'center',
-        fontSize: 25,
-        color: '#5B352C',
-        marginBottom: 25,
-      },
-  
-      textoNome: {
-        fontSize: 18,
-        color: '#5B352C',
-        // fontWeight: 'regular',
-      },
-  
-      input: {
-        backgroundColor: '#F2E7C7',
-        color: '#5B352C',
-        height: 43,
-        padding: 10,
-        marginBottom: 5,
-        marginTop: 5,
-        placeholderTextColor: '#5B352C',
-      },
-  
-      checkboxTermos: {
-        flexDirection: 'row',
-        alignItems: 'center'
-      },
-  
-      botao: {
-        height: 43,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#5B352C',
-        marginTop: 50,
-        marginBottom: 20,
-      },
-  
-      botaoTexto: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: '#D8A35D'
-      },
-  
-    });
+  container: {
+    backgroundColor: '#D8A35D',
+  },
+
+  btnvoltar: {
+    height: 26,
+    width: 26,
+    marginVertical: 20,
+    marginStart: 10,
+  },
+
+  containerFormulario: {
+    backgroundColor: '#FFF',
+    paddingHorizontal: 35,
+    borderTopStartRadius: 20,
+    borderTopEndRadius: 20,
+  },
+
+  areaFormulario: {
+    flexDirection: 'column',
+    backgroundColor: '#FFF',
+    flex: 1,
+  },
+
+  title: {
+    textAlign: 'center',
+    fontSize: 25,
+    color: '#5B352C',
+    marginVertical: 25,
+  },
+
+  doisForms: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  textoNome: {
+    fontSize: 18,
+    color: '#5B352C',
+    marginStart: 10,
+  },
+
+  input: {
+    backgroundColor: '#F2E7C7',
+    color: '#5B352C',
+    height: 43,
+    padding: 10,
+    marginBottom: 5,
+    marginTop: 5,
+    placeholderTextColor: '#5B352C',
+    fontSize: 18,
+  },
+
+  inputDois: {
+    backgroundColor: '#F2E7C7',
+    color: '#5B352C',
+    height: 43,
+    padding: 10,
+    marginBottom: 5,
+    marginTop: 5,
+    placeholderTextColor: '#5B352C',
+    fontSize: 18,
+    width: '49%',
+  },
+
+  switchTermos: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 15,
+  },
+
+  botao: {
+    height: 43,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#5B352C',
+    marginTop: 50,
+    marginBottom: 20,
+  },
+
+  botaoTexto: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#D8A35D'
+  },
+
+});
